2bfdbd847c224cfef9aa2b02c066e184fbad0a04
Nikhil <nikhil@Johns-iMac.local>
2012-08-01 16:56:54 -0500

    add responses for no sender

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ NeverForgetify::Application.routes.draw do
   devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout'}
   devise_scope :user do
     get "/login" => "devise/sessions#new"
-    get "/logout" => "devise/sessions#destory"
+    get "/logout" => "devise/sessions#destroy"
     
   end
   
