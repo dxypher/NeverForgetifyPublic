d17ce74acf3f9c2465461615ecaabb2d96a06680
Nikhil <nikhil@Johns-iMac.local>
2012-08-01 16:52:58 -0500

    add responses for no sender

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 NeverForgetify::Application.routes.draw do
   get "twilio/create"
 
-  devise_for :users
+  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout'}
 
   root to: "notifications#index"
 
